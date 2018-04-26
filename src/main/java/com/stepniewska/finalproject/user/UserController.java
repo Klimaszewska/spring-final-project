69d04da5d84a8a2d5dd0315baf26af246d8562ef
Klimaszewska <klimaszewska.justyna@gmail.com>
2018-04-26 22:05:45 +0200

    A method for displaying lists has been added.

--- src/main/java/com/stepniewska/finalproject/user/UserController.java
+++ src/main/java/com/stepniewska/finalproject/user/UserController.java
@@ -1,14 +1,20 @@
 package com.stepniewska.finalproject.user;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/unauth/credentials")
     public String getCredentials() {
@@ -23,8 +29,21 @@ public class UserController {
 
 
     @PostMapping("/auth/login")
-    public boolean login() {
-        return true;
+    public String displayLoginMessage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String result = "Welcome, " + userRepository.findByEmail(authentication.getName()).getDisplayedUserName() + "!";
+        return result;
+    }
+
+    // just a sample method for a list return - will be removed
+    @GetMapping("/")
+    public List<String> displayUserList(){
+        List<User> inputUserList = userRepository.findAll();
+        List<String> displayedUserList = new ArrayList<>();
+        for (User user : inputUserList) {
+                displayedUserList.add(user.getDisplayedUserName() + "," + user.getEmail());
+        }
+        return displayedUserList;
     }
 
 }
